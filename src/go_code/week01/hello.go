76eeab4ba7bd40f95424e46b67f20bd7beeaf78a
eternalstop <wlhuijiadehaizi@163.com>
2022-09-06 22:22:44 +0800

    modified:   src/go_code/week01/hello.go 	new file:   src/go_code/week02/myecho.go

--- src/go_code/week01/hello.go
+++ src/go_code/week01/hello.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
+	var x int
+	x = 1
+	var y int
+	y = 2
+	swap(&x, &y)
+	fmt.Println(x, y)
 	fmt.Println("hello, golang world!!!")
+}
+
+func swap(p *int, q *int) {
+	var t = *p
+	*p = *q
+	*q = t
 }
