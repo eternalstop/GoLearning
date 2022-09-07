63cb9e619dbd417c6bedc199b2826a818d3d36f2
wangliang <eternalstop7@gmail.com>
2022-09-07 14:27:21 +0800

    调整目录层级

---
+++ go_code/week01/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int
+	x = 1
+	var y int
+	y = 2
+	swap(&x, &y)
+	fmt.Println(x, y)
+	fmt.Println("hello, golang world!!!")
+}
+
+func swap(p *int, q *int) {
+	var t = *p
+	*p = *q
+	*q = t
+}
